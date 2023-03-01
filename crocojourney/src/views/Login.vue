cd5caad11beb4ca9b81d39972219395d5384b087
MatthieuGalante <matthieu.galante5@etu.univ-lorraine.fr>
2023-03-01 21:23:01 +0100

    ajout des bases du login

--- crocojourney/src/views/Login.vue
+++ crocojourney/src/views/Login.vue
@@ -1,6 +1,47 @@
 <template>
-    <h1>Login</h1>
-    <p>
-        Login page
-    </p>
+    <div class="container text-center">
+  <div class="row">
+    <div style="width: 25%;">
+        <p>column</p>
+    </div>
+    <div class="col">
+        <div class="row">
+            <div class="h-25">
+                <p>column</p>
+            </div>
+        </div>
+        <div class="row" style="padding-top: 10%;">
+            <div class="rounded" style="border-width: thick; border: solid ; border-color: #008c46;">
+                <h2 style="padding-top: 10%;">Se connecter</h2>
+                <img src="../assets/img/crocojourney.png" alt="" style="width: 40%;">
+                <div class="row">
+                    <div></div>
+                    <div><input type="text" id="login" name="login"></div>
+                    <div></div>
+                    
+                </div>
+                <div class="row">
+                    <div></div>
+                    <div style="padding-top: 3%;"><input type="text" id="password" name="password"></div>
+                    <div></div>
+                </div>
+                <div style="padding-top: 13%;">
+                    <a href="">S'inscire</a>
+                </div>
+                
+                <div style="padding-top: 3%; padding-bottom: 7%;">
+                    <button class="btn btn-success" >Se connecter</button>
+                </div>
+                
+            </div>
+        </div>
+        <div class="row">
+            <p>column</p>
+        </div>
+    </div>
+    <div style="width: 25%;">
+        <p>column</p>
+    </div>
+  </div>
+</div>
 </template>

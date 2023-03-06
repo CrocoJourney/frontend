585025bf49e3abfdb5f804017f269f231b7e79d3
Phil1010 <philliptysoun@gmail.com>
2023-03-07 00:54:22 +0100

    Mise à jour page d'accueil
    Ajout du composant searchBar et autocomplétion.

--- crocojourney/src/views/Home.vue
+++ crocojourney/src/views/Home.vue
@@ -1,30 +1,38 @@
 <template>
-    <div class="d-flex flex-column align-items-center">
-        <div class="d-flex align-items-center">
-            <div class="mr-3">
-                <h3 class="font-weight-bold">Argent. Ami. Rejoins CrocoJourney</h3>
-                <p>Deviens un de nos membres pour proposer ou rejoindre un trajet.</p>
-
+    <div class="container my-3">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-md-6">
+                <h3 class="font-weight-bold mb-3">Argent. Ami. Rejoins CrocoJourney</h3>
+                <p class="mb-4">Deviens un de nos membres pour proposer ou rejoindre un trajet.</p>
                 <div v-if="!User.isLoggedIn()">
                     <RouterLink to="register">
-                        <button class="btn btn-success" style="border-radius: 5px">S'inscrire maintenant</button>
+                        <button class="btn btn-success btn-lg mb-4" style="border-radius: 5px">
+                            S'inscrire maintenant
+                        </button>
                     </RouterLink>
                 </div>
-
+                <div v-else>
+                    <SearchBar />
+                </div>
             </div>
-            <img src="../assets/img/favicon.ico" alt="Description de l'image" class="w-50 h-auto" />
+            <div class="col-md-6">
+                <img src="../assets/img/favicon.ico" alt="Description de l'image" class="w-100 h-auto" />
+            </div>
         </div>
     </div>
 </template>
-
 <script>
-import { defineComponent } from 'vue'
-import User from "../scripts/User.js"
+import { defineComponent } from 'vue';
+import User from '../scripts/User.js';
+import SearchBar from '../components/SearchBar.vue';
 export default defineComponent({
     name: 'Home',
+    components: {
+        SearchBar,
+    },
     setup() {
         return {
-          User
+            User,
         };
     },
 });

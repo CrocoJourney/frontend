ed6e13125fdb421e33494d14916a2d8245520ea2
Juliette Gaillemin <juliette.gaillemin3@etu.univ-lorraine.fr>
2023-03-08 22:01:04 +0100

    script SearchResults

--- crocojourney/src/components/SearchResults.vue
+++ crocojourney/src/components/SearchResults.vue
@@ -9,10 +9,10 @@
           <div class="container text-center">
               <!-- Stack the columns on mobile by making one full-width and the other half-width -->
               <div class="row mb-4">
-                <div class="col-6 col-md-5 text-end fs-3 fw-bold">Nancy</div>
+                <div class="col-6 col-md-5 text-end fs-3 fw-bold">{{ departure }}</div>
                 <div class="col-1 text-center fs-3 fw-bold">à</div>
-                <div class="col-1 text-start fs-3 fw-bold">Metz</div>
-                <div class="col-4 fs-3 fw-semibold">08:00</div>
+                <div class="col-1 text-start fs-3 fw-bold">{{ arrival }}</div>
+                <div class="col-4 fs-3 fw-semibold">{{time}}</div>
               </div>
             
               <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
@@ -25,20 +25,22 @@
             
               <!-- Columns are always 50% wide, on mobile and desktop -->
               <div class="row mt-4">
-                <div class="col-6 col-md-3 text-end fs-5">Bob</div>
+                <div class="col-6 col-md-3 text-end fs-5">{{name}}</div>
 
                 <!--NOTE-->
-                <div class="col-1 fs-5 fw-lighter text-end">4.5</div>
+                <div class="col-1 fs-5 fw-lighter text-end">{{rate}}</div>
                 <div class="col-1 fs-5 fw-lighter text-start">/5</div>
 
-                <div class="col-5 text-end fs-1 fw-bolder">10</div>
+                <div class="col-5 text-end fs-1 fw-bolder">{{price}}</div>
                 <div class="col-1 text-start fs-1 fw-bolder">€</div>
 
               </div>
             </div>
 
-        <RouterLink to="#" class="btn btn-primary mt-4 mb-2">Ajouter</RouterLink>
+        <RouterLink to="#" class="btn btn-primary mt-4 mb-2">Accepter</RouterLink>
       </div>
+
+      <SearchResults arrival="Lyon" departure="Paris"  date="2023-04-10"  name="Bob"  rate="4.5"   price="10"></SearchResults>
     </div>
 
 
@@ -65,3 +67,38 @@
 
     
 </template>
+
+<script>
+  import { defineComponent, DefineComponent } from 'vue';
+  export default defineComponent({
+    name: 'SearchResults',
+    props: {
+      departure: {
+        type: String,
+        required: true
+      },
+      arrival: {
+        type: String,
+        required: true
+      },
+      time: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      rate: {
+        type: String,
+        required: true
+      },
+      price: {
+        type: String,
+        required: true
+      },
+
+    },
+  });
+
+</script>

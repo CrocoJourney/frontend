25d9f5f9dedcb227c0776af31d47b0e538cfe0f3
Yoan Nougué-Ruiz <yoan.nougueruiz@gmail.com>
2023-03-08 17:21:34 +0100

    Préparation modal d'annulation de trajet

---
+++ crocojourney/src/views/CancelJourney.vue
@@ -0,0 +1,28 @@
+<template>
+    <!--<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>-->
+
+<!-- Modal -->
+<div class="modal fade" id="cancelJourneyModal" tabindex="-1" aria-labelledby="cancelJourneyModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="cancelJourneyModalLabel">Annuler un trajet</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          Voulez-vous vraiment annuler ce trajet?
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+          <button type="button" class="btn btn-danger">Supprimer le trajet</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>

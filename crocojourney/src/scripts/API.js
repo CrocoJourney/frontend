3f8be1b9da565564fbeb49a8dd768f5e0cddace9
toxicbloud <fanta.r55000@gmail.com>
2023-03-15 20:32:53 +0100

    modif API.js

--- crocojourney/src/scripts/API.js
+++ crocojourney/src/scripts/API.js
@@ -1,6 +1,7 @@
 import User from './User.js';
 class API {
-    static API_URL = import.meta.env.DEV ? 'http://localhost:8000' : 'https://crocojourney-api.antoninrousseau.fr';
+    static API_URL =
+        import.meta.env.DEV ? 'http://localhost:8000' : 'https://crocojourney-api.antoninrousseau.fr';
 
     /**
      * @typedef {string} METHOD
@@ -16,29 +17,50 @@ class API {
         DELETE: 'DELETE',
         PATCH: 'PATCH',
     };
+
+    /**
+     * @typedef {string} CONTENT_TYPE
+     */
+
+    /**
+     * @enum {CONTENT_TYPE}
+     */
+    static CONTENT_TYPE = {
+        JSON: 'application/json',
+        URL_ENCODED: 'application/x-www-form-urlencoded',
+        FORM_DATA: 'multipart/form-data'
+    };
+
     static get AuthorizationHeader() {
         return 'x-crocojourney-authorization';
     }
+
     /**
      *
      * @param {METHOD} method
-     * @param {*} url
-     * @param {*} data
-     * @param {*} headers
+     * @param {String} url route de l'API
+     * @param {*} body corps de la requête
+     * @param {*} headers headers à rajouter à la requête
+     * @param {CONTENT_TYPE} type du body 
      */
-    static request(method, url, body, headers) {
+    static request(method, url, body, headers, contentType = this.CONTENT_TYPE.JSON) {
         return new Promise(async (resolve, reject) => {
+            const headerConstructor = new Headers();
+            headerConstructor.append("Sec-Fetch-Mode", "cors");
+            headerConstructor.append("Sec-Fetch-Site", "same-origin");
+            headerConstructor.append("Sec-Fetch-Dest", "empty");
+            headerConstructor.append("Referer", window.location.origin);
+            headerConstructor.append("Content-Type", contentType);
+            if (headers == this.CONTENT_TYPE.FORM_DATA) {
+                headerConstructor.delete("Content-Type")
+            }
+            headerConstructor.append("Accept", "application/json");
+            for (const key in headers) {
+                headerConstructor.append(key, headers[key]);
+            }
             const response = await fetch(`${API.API_URL}${url}`, {
                 method: method,
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                    'Sec-Fetch-Mode': 'cors',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-Dest': 'empty',
-                    Referer: window.location.origin,
-                    ...headers,
-                },
+                headers: headerConstructor,
                 mode: 'cors',
                 body: body,
             });
@@ -58,25 +80,28 @@ class API {
      *
      * @param {METHOD} method
      * @param {string} url /users ...
-     * @param {*} data body of the request maybe json or form data etc..
+     * @param {*} body body of the request maybe json or form data etc..
+     * @param {CONTENT_TYPE} contentType type du body , utilisé dans le header de la requete
      * @returns
      */
-    static requestLogged(method, url, body) {
+    static requestLogged(method, url, body, contentType) {
         return new Promise(async (resolve, reject) => {
             try {
                 // on fait la requete avec le token en header
                 const res = await API.request(method, url, body, {
                     [API.AuthorizationHeader]: `Bearer ${User.currentUser.accessToken}`,
-                });
+                }, contentType);
                 resolve(res);
             } catch (err) {
-                console.log('requestLogged error');
                 // si on a une erreur 401 on refresh les tokens
                 switch (err.status) {
                     case 401:
+                        console.log("faut qu'on refresh");
                         try {
                             await API.refreshTokens();
+                            console.log("ah pas refresh comme i faut");
                         } catch (err) {
+                            console.log("ah ptn");
                             reject(err);
                         }
                         // on refait la requete avec le nouveau token
@@ -97,24 +122,25 @@ class API {
             }
         });
     }
+
     static refreshTokens() {
         return new Promise((resolve, reject) => {
             fetch(`${API.API_URL}/auth/refresh`, {
-                method: API.METHOD.POST,
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json',
-                    'Sec-Fetch-Mode': 'cors',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-Dest': 'empty',
-                    Referer: window.location.origin,
-                },
-                mode: 'cors',
-                // add refresh_token to body in json
-                body: JSON.stringify({
-                    refresh_token: `${User.currentUser.refreshToken}`,
-                }),
-            })
+                    method: API.METHOD.POST,
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Accept: 'application/json',
+                        'Sec-Fetch-Mode': 'cors',
+                        'Sec-Fetch-Site': 'same-origin',
+                        'Sec-Fetch-Dest': 'empty',
+                        Referer: window.location.origin,
+                    },
+                    mode: 'cors',
+                    // add refresh_token to body in json
+                    body: JSON.stringify({
+                        refresh_token: `${User.currentUser.refreshToken}`,
+                    }),
+                })
                 .then((response) => {
                     switch (response.status) {
                         case 200:
@@ -138,56 +164,37 @@ class API {
                 });
         });
     }
-    static login(username, password) {
-        return new Promise((resolve, reject) => {
-            fetch(`${API.API_URL}/auth/login`, {
-                method: API.METHOD.POST,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    Accept: 'application/json',
-                    'Sec-Fetch-Mode': 'cors',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-Dest': 'empty',
-                    Referer: window.location.origin,
-                },
-                mode: 'cors',
-                body: `username=${username}&password=${password}`,
-            })
-                .then((response) => {
-                    if (response.status === 200) {
-                        response.json().then(async (data) => {
-                            User.currentUser = new User();
-                            User.currentUser.accessToken = data.access_token;
-                            User.currentUser.refreshToken = data.refresh_token;
-                            await User.currentUser.fetchInfo();
-                            User.saveToLocalStorage();
-                            resolve(data);
-                        });
-                    } else {
-                        reject(response);
-                    }
-                })
-                .catch((err) => {
-                    reject(err);
-                });
-        });
+
+    static async login(username, password) {
+        try {
+            const body = `username=${username}&password=${password}`;
+            const res = await API.request(API.METHOD.POST, "/auth/login", body, undefined, API.CONTENT_TYPE.URL_ENCODED);
+            User.currentUser = new User();
+            User.currentUser.accessToken = res.access_token;
+            User.currentUser.refreshToken = res.refresh_token;
+            await User.currentUser.fetchInfo();
+            User.saveToLocalStorage();
+        } catch (error) {
+            const json = await error.json();
+            throw new Error(json.detail);
+        }
     }
 
-    static createJourney(firstname, lastname, mail, password, confirmPassword ) {
+    static createJourney(firstname, lastname, mail, password, confirmPassword) {
         return new Promise((resolve, reject) => {
             fetch(`${API.API_URL}/auth/login`, {
-                method: API.METHOD.POST,
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    Accept: 'application/json',
-                    'Sec-Fetch-Mode': 'cors',
-                    'Sec-Fetch-Site': 'same-origin',
-                    'Sec-Fetch-Dest': 'empty',
-                    Referer: window.location.origin,
-                },
-                mode: 'cors',
-                body: `username=${username}&password=${password}`,
-            })
+                    method: API.METHOD.POST,
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Accept: 'application/json',
+                        'Sec-Fetch-Mode': 'cors',
+                        'Sec-Fetch-Site': 'same-origin',
+                        'Sec-Fetch-Dest': 'empty',
+                        Referer: window.location.origin,
+                    },
+                    mode: 'cors',
+                    body: `username=${username}&password=${password}`,
+                })
                 .then((response) => {
                     if (response.status === 200) {
                         response.json().then(async (data) => {
--- crocojourney/src/views/Login.vue
+++ crocojourney/src/views/Login.vue
@@ -1,58 +1,59 @@
 <template>
     <div class="container text-center">
-  <div class="row">
-    <div style="width: 30%;">
-        <p></p>
-    </div>
-    <div class="col">
         <div class="row">
-            <div class="h-25">
+            <div style="width: 30%;">
+                <p></p>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="h-25">
+                        <p></p>
+                    </div>
+                </div>
+                <div class="row" style="padding-top: 10%;">
+                    <div class="rounded" style="border-width: thick; border: solid ; border-color: #008c46;">
+                        <h2 style="padding-top: 10%;">Se connecter</h2>
+                        <img src="../assets/img/crocojourney.png" alt="" style="width: 40%;">
+                        <div class="row">
+                            <div></div>
+                            <div class="col-md-8 mx-auto form-floating">
+                                <input ref="login" class="form-control" type="text" id="login" name="login"
+                                    placeholder="Login">
+                                <label for="login" class="form-label">Mail</label>
+                            </div>
+                            <div></div>
+
+                        </div>
+                        <div class="row">
+                            <div style="padding-top: 5%;"></div>
+                            <div class="col-md-8 mx-auto form-floating">
+                                <input ref="password" class="form-control col-md-8" type="password" id="password"
+                                    name="password" placeholder="password">
+                                <label for="password" class="form-label">Mot de passe</label>
+                            </div>
+                            <div></div>
+                        </div>
+
+                        <div style="padding-top: 13%;">
+                            <RouterLink class="" to="/recovery">Mot de passe oublié ?</RouterLink>
+                        </div>
+                        <div style="padding-top: 3%;">
+                            <RouterLink class="" to="/register">S'inscrire</RouterLink>
+                        </div>
+                        <div style="padding-top: 3%; padding-bottom: 7%;">
+                            <button @click="login" class="btn btn-success">Se connecter</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <p></p>
+                </div>
+            </div>
+            <div style="width: 30%;">
                 <p></p>
             </div>
         </div>
-        <div class="row" style="padding-top: 10%;">
-            <div class="rounded" style="border-width: thick; border: solid ; border-color: #008c46;">
-                <h2 style="padding-top: 10%;">Se connecter</h2>
-                <img src="../assets/img/crocojourney.png" alt="" style="width: 40%;">
-                <div class="row">
-                    <div></div>
-                        <div class="col-md-8 mx-auto form-floating">
-                            <input ref="login" class="form-control" type="text" id="login" name="login" placeholder="Login">
-                            <label for="login" class="form-label ms-1">Mail</label>
-                        </div>
-                    <div></div>
-                    
-                </div>
-                <div class="row">
-                    <div style="padding-top: 5%;"></div>
-                        <div class="col-md-8 mx-auto form-floating">
-                            <input ref="password" class="form-control col-md-8" type="password" id="password" name="password" placeholder="password">
-                            <label for="password" class="form-label ms-1">Mot de passe</label>
-                        </div>
-                    <div></div>
-                </div>
-                
-                <div style="padding-top: 13%;">
-                    <RouterLink class="" to="/recovery">Mot de passe oublié ?</RouterLink>
-                </div>
-                <div style="padding-top: 3%;">
-                    <RouterLink class="" to="/register">S'inscrire</RouterLink>
-                </div>
-                <div style="padding-top: 3%; padding-bottom: 7%;">
-                    <button @click="login" class="btn btn-success" >Se connecter</button>
-                </div>
-                
-            </div>
-        </div>
-        <div class="row">
-            <p></p>
-        </div>
     </div>
-    <div style="width: 30%;">
-        <p></p>
-    </div>
-  </div>
-</div>
 </template>
 
 <script>
@@ -60,21 +61,21 @@ import { defineComponent } from 'vue';
 import API from "../scripts/API.js"
 import emitter from "../scripts/emitter.js"
 export default defineComponent({
-    name: 'Login',
+    name: "Login",
     methods: {
         async login() {
             const login = this.$refs.login.value;
             const password = this.$refs.password.value;
-            if(!login || !password) return window.alert("Veuillez remplir tous les champs");
+            if (!login || !password)
+                return window.alert("Veuillez remplir tous les champs");
             try {
                 await API.login(login, password);
                 // on envoie un event pour dire que l'utilisateur s'est connecté pour mettre à jour la navbar
-                emitter.emit('userUpdated');
-                this.$router.push({ path: '/' });
-            } catch (error) {
-                const json = await error.json();
-                console.log(json);
-                window.alert(json.detail);
+                emitter.emit("userUpdated");
+                this.$router.go(-1);
+            }
+            catch (error) {
+                window.alert(error);
             }
         }
     }

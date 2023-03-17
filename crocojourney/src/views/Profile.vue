af2f7d37bca7629c6499d7b69ecfdc72e927c812
Phil1010 <philliptysoun@gmail.com>
2023-03-17 06:21:41 +0100

    Profile.vue relié avec l'API

--- crocojourney/src/views/Profile.vue
+++ crocojourney/src/views/Profile.vue
@@ -4,141 +4,124 @@
         <br />
         <h1>Mon Compte</h1>
         <br /><br />
-        <form action="">
-            <!--Nom/Prenom-->
-            <div class="mt-5 mb-3">
-                <label for="formNom" class="form-label">Nom*:</label>
-                <input :value="lastName" ref="lastName" type="text" class="form-control" id="formNom" />
-            </div>
-            <div class="mb-5">
-                <label for="formPrenom" class="form-label">Prénom*:</label>
-                <input :value="firstName" ref="firstName" type="text" class="form-control" id="formPrenom" />
-            </div>
-
-            <!--Genre-->
-            <fieldset class="mb-5">
-                <legend class="col-form-label col-sm-1 pt-0">Genre:</legend>
-                <div class="col-sm-4">
-                    <div class="form-check">
-                        <input
-                            ref="women"
-                            class="form-check-input"
-                            type="radio"
-                            name="genreRadios"
-                            id="RadioFemme"
-                            checked
-                        />
-                        <label class="form-check-label" for="RadioFemme"> Femme </label>
-                    </div>
-                    <div class="form-check">
-                        <input ref="men" class="form-check-input" type="radio" name="genreRadios" id="Radiohomme" />
-                        <label class="form-check-label" for="Radiohomme"> Homme </label>
-                    </div>
-                </div>
-            </fieldset>
-
-            <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
-
-            <!--Photo-->
-            <div class="mt-5 mb-3">
-                <label for="formPhoto" class="form-label">Photo :</label>
-                <img :src="photoPath" class="img-thumbnail rounded" alt="Photo de profil" />
-            </div>
-            <div class="mb-5">
-                <label for="formFileSm" class="form-label"></label>
-                <input
-                    @change="photoHandler"
-                    ref="photoInput"
-                    class="form-control form-control-sm"
-                    id="formFileSm"
-                    type="file"
-                />
-            </div>
-
-            <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
-
-            <!--Email/Telephone-->
-            <div class="mt-5 mb-3">
-                <label for="formEmail" class="form-label">Email*:</label>
-                <input :value="email" ref="email" type="text" class="form-control" id="formEmail" />
-            </div>
-            <div class="mb-5">
-                <label for="formTelephone" class="form-label">Numéro de Téléphone*:</label>
-                <input :value="phone" ref="phone" type="text" class="form-control" id="formTelephone" />
-            </div>
-
-            <!--Notifs-->
-
-            <div class="mb-4 form-check form-switch">
-                <input
-                    :value="notif"
-                    ref="notif"
-                    class="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    id="switchNotifs"
-                />
-                <label class="form-check-label" for="switchNotifs">Voulez-vous activer les notifications ?</label>
-            </div>
-
-            <!--Voiture-->
-            <div class="mb-4 form-check form-switch">
-                <input
-                    :value="vehicle"
-                    ref="vehicle"
-                    class="form-check-input"
-                    type="checkbox"
-                    role="switch"
-                    id="switchVoiture"
-                />
-                <label class="form-check-label" for="switchVoiture">Possédez-vous un véhicule ?</label>
-            </div>
-
-            <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
-
-            <!--Mot de passe-->
-            <div class="mt-5">
-                <label for="pass" class="form-label">Mot de Passe*: (8 caractères minimum):</label>
-                <input
-                    ref="passInput"
-                    type="password"
-                    class="form-control"
-                    id="pass"
-                    name="password"
-                    minlength="8"
-                    required
-                />
-            </div>
-            <div class="mt-3 mb-5">
-                <label for="pass2" class="form-label">Confirmation du Mot de Passe*:</label>
-                <input
-                    ref="pass2Input"
-                    type="password"
-                    class="form-control"
-                    id="pass2"
-                    name="password"
-                    minlength="8"
-                    required
-                />
-            </div>
-
-            <!--Bouton Valider-->
-            <button @click="updateProfile" type="submit" class="btn btn-success col-md-3 mb-4">Valider</button>
-        </form>
-    </div>
-
-    <p>* champs obligatoires</p>
-
-    <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
-
-    <div class="col-md-5 mx-auto text-center">
-        <br />
-        <h1>Modifier mon Mot de Passe</h1>
-        <br /><br />
-
+        <!--Nom/Prenom-->
         <div class="mt-5 mb-3">
-            <RouterLink class="btn btn-success" to="/resetpassword">Modifier</RouterLink>
+            <label for="formNom" class="form-label">Nom:</label>
+            <input ref="lastName" type="text" class="form-control" id="formNom" v-model.value="lastName" />
         </div>
+        <div class="mb-5">
+            <label for="formPrenom" class="form-label">Prénom:</label>
+            <input v-model.value="firstName" ref="firstName" type="text" class="form-control" id="formPrenom" />
+        </div>
+
+        <!--Genre-->
+        <fieldset class="mb-5">
+            <legend class="col-form-label col-sm-1 pt-0">Genre:</legend>
+            <div class="col-sm-4">
+                <div class="form-check">
+                    <input
+                        ref="women"
+                        class="form-check-input form-check-success"
+                        type="radio"
+                        name="genreRadios"
+                        id="RadioFemme"
+                        checked
+                        v-model="sex"
+                        value="F"
+                    />
+                    <label class="form-check-label" for="RadioFemme"> Femme </label>
+                </div>
+                <div class="form-check">
+                    <input
+                        ref="men"
+                        class="form-check-input"
+                        type="radio"
+                        name="genreRadios"
+                        id="Radiohomme"
+                        v-model="sex"
+                        value="H"
+                    />
+                    <label class="form-check-label" for="Radiohomme"> Homme </label>
+                </div>
+            </div>
+        </fieldset>
+
+        <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
+
+        <!--Photo-->
+        <div class="mt-5 mb-3">
+            <label for="formPhoto" class="form-label">Photo :</label>
+            <img :src="photoPath" class="img-thumbnail rounded w-25 h-25" alt="Photo de profil" />
+        </div>
+        <div class="mb-5">
+            <label for="formFileSm" class="form-label"></label>
+            <input
+                @change="photoHandler"
+                ref="photoInput"
+                class="form-control form-control-sm"
+                id="formFileSm"
+                type="file"
+            />
+        </div>
+
+        <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
+
+        <!--Email/Telephone-->
+        <div class="mt-5 mb-3">
+            <label for="formEmail" class="form-label">Email:</label>
+            <input v-model.value="email" ref="email" type="text" class="form-control" id="formEmail" />
+        </div>
+        <div class="mb-5">
+            <label for="formTelephone" class="form-label">Numéro de Téléphone:</label>
+            <input v-model.value="phone" ref="phone" type="text" class="form-control" id="formTelephone" />
+        </div>
+
+        <!--Notifs-->
+
+        <div class="mb-4 form-check form-switch">
+            <input
+                :value="notif"
+                ref="notif"
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="switchNotifs"
+                v-model="notif"
+            />
+            <label class="form-check-label" for="switchNotifs">Voulez-vous activer les notifications ?</label>
+        </div>
+
+        <!--Voiture-->
+        <div class="mb-4 form-check form-switch">
+            <input
+                :value="vehicule"
+                ref="vehicule"
+                class="form-check-input"
+                type="checkbox"
+                role="switch"
+                id="switchVoiture"
+                v-model="vehicule"
+            />
+            <label class="form-check-label" for="switchVoiture">Possédez-vous un véhicule ?</label>
+        </div>
+
+        <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
+
+        <!-- Mot de passe -->
+        <div class="mt-5">
+            <label for="pass" class="form-label">Nouveau mot de passe :</label>
+            <input ref="passInput" type="password" class="form-control" id="pass" name="password" minlength="8" />
+        </div>
+        <div class="mt-3 mb-5">
+            <label for="pass2" class="form-label">Confirmation du nouveau mot de passe :</label>
+            <input ref="pass2Input" type="password" class="form-control" id="pass2" name="password" minlength="8" />
+        </div>
+
+        <!--Bouton Valider-->
+        <button type="button" class="btn btn-success col-md-3 mb-2 mt-2" @click="updateProfile">Valider</button>
+
+        <!-- Message d'alerte -->
+        <div class="alert alert-success" role="alert" v-if="showAlert">Vos informations ont bien été modifiés!</div>
     </div>
 
     <div class="h4 pb-5 mb-4 mx-auto col-md-8 text-light border-bottom border-light border-2"></div>
@@ -174,12 +157,11 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn btn-primary" onclick="deleteAccount()">Supprimer</button>
+                    <button @click="deleteAccount" class="btn btn-success">Supprimer</button>
                 </div>
             </div>
         </div>
     </div>
-    <p>* champs obligatoires</p>
 </template>
 <script>
 import API from '../scripts/API';
@@ -197,26 +179,89 @@ export default {
             email: User.currentUser.mail,
             phone: User.currentUser.phonenumber,
             notif: User.currentUser.mailNotification,
-            vehicle: User.currentUser.vehicle,
+            vehicule: User.currentUser.car,
+            sex: User.currentUser.sex,
+            password: User.currentUser.password,
+            confirmPassword: User.currentUser.password,
+            showAlert: false,
         };
     },
     methods: {
+        async deleteAccountBis(event) {
+            API.requestLogged(API.METHOD.DELETE, `/users/${User.currentUser.id}`, undefined, API.CONTENT_TYPE.JSON);
+        },
+        async deleteAccount(event) {
+            await this.deleteAccountBis();
+            User.currentUser = null;
+            User.saveToLocalStorage(User.currentUser);
+            emitter.emit('userUpdated');
+            this.$router.replace('/');
+            await this.redirectToHomePage();
+        },
+        async redirectToHomePage() {
+            await this.$router.push('/');
+            await this.$router.go();
+        },
+
         photoHandler(event) {
             this.photoPath = window.URL.createObjectURL(event.target.files[0]);
         },
         async updateProfile(event) {
+            this.showAlert = true;
+
+            // Masquer l'alerte après 2 secondes
+            setTimeout(() => {
+                this.showAlert = false;
+            }, 2000);
             const formDatas = new FormData();
             formDatas.append('photo', this.$refs.photoInput.files[0]);
+
+            const userData = {
+                firstname: this.$refs.firstName.value,
+                lastname: this.$refs.lastName.value,
+                mail: this.$refs.email.value,
+                phonenumber: this.$refs.phone.value,
+                sex: this.sex,
+                car: this.vehicule,
+                mailNotification: this.notif,
+            };
+
+            // si le password a été modifié on ajoute dans userData le nouveau password
+            if (this.$refs.passInput.value !== '') {
+                if (this.$refs.passInput.value.length < 8) {
+                    window.alert('Le mot de passe doit contenir au moins 8 caractères');
+                } else if (this.$refs.passInput.value === this.$refs.pass2Input.value) {
+                    userData.password = this.$refs.passInput.value;
+                    userData.confirmPassword = this.$refs.pass2Input.value;
+                } else {
+                    window.alert('Les mots de passe ne correspondent pas');
+                }
+            }
             try {
                 const res = await API.requestLogged(
-                    API.METHOD.POST,
-                    '/users/me/profilePicture',
-                    formDatas,
-                    API.CONTENT_TYPE.FORM_DATA
+                    API.METHOD.PATCH,
+                    '/users/me',
+                    JSON.stringify(userData),
+                    API.CONTENT_TYPE.JSON
                 );
-            } catch (e) {
-                window.alert('Erreur lors de la mise à jour de la photo de profil');
+            } catch (error) {
+                window.alert('Erreur lors de la mise à jour des infos de profil');
             }
+
+            // si la photo a été modifiée on la met à jour
+            if (this.$refs.photoInput.files.length > 0) {
+                try {
+                    const res = await API.requestLogged(
+                        API.METHOD.POST,
+                        '/users/me/profilePicture',
+                        formDatas,
+                        API.CONTENT_TYPE.FORM_DATA
+                    );
+                } catch (e) {
+                    window.alert('Erreur lors de la mise à jour de la photo de profil');
+                }
+            }
+
             await User.currentUser.fetchInfo();
             User.saveToLocalStorage();
             emitter.emit('userUpdated');

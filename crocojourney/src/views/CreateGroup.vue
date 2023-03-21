645785080d405d90fae334643a1cbf9eea6db0dc
yoan-ngr <yoan.nougueruiz@gmail.com>
2023-03-21 18:46:47 +0100

    Réparation inscription

--- crocojourney/src/views/CreateGroup.vue
+++ crocojourney/src/views/CreateGroup.vue
@@ -2,20 +2,24 @@
     <div class="col-md-8 mx-auto pt-5">
         <h1 class="text-center">Créer un nouveau groupe</h1>
         <br><br>
-        <div class="form-floating mx-auto col-md-8 mb-3">
-            <input type="text" class="form-control" id="groupName" placeholder="Mon super groupe">
+        <div class="form-floating mx-auto col-md-8 mb-3 input-group has-validation">
+            <input ref="groupName" type="text" class="form-control" id="groupName" placeholder="Mon super groupe">
             <label for="groupName">Nom du groupe</label>
+            <div class="invalid-feedback">
+                Veuillez entrer un nom de groupe valide, constitué d'au moins 3 caractères.
+            </div>
         </div>
 
         <div class="row mt-5">
             <div class="col px-5">
                 <h3 class="text-center mb-3">Membres actuels</h3>
                 <ul class="list-group list-group-flush mt-3">
-                    <li class="list-group-item fade show d-flex">An item <button type="button" class="btn-close ms-auto " aria-label="Close"></button></li>
+                    <p class="text-muted text-center">Vous n'avez pour l'instant ajouté personne au groupe. Utilisez la liste de droite pour ajouter des amis !</p>
+                    <!--<li class="list-group-item fade show d-flex">An item <button type="button" class="btn-close ms-auto " aria-label="Close"></button></li>
                     <li class="list-group-item">A second item</li>
                     <li class="list-group-item">A third item</li>
                     <li class="list-group-item">A fourth item</li>
-                    <li class="list-group-item">And a fifth one</li>
+                    <li class="list-group-item">And a fifth one</li>-->
                 </ul>
             </div>
             <div class="col px-5">
@@ -32,6 +36,8 @@
 
         <br><br>
 
+        <p ref="test"></p>
+
         <div class="mx-auto col-md-2">
             <input name="" id="" class="btn btn-success" type="button" value="Créer le groupe">
         </div>
@@ -39,4 +45,28 @@
 </template>
 
 <script>
+    import { defineComponent } from 'vue';
+    import API from "../scripts/API.js"
+    export default defineComponent({
+        name: 'GetUsers',
+        methods: {
+            async get_users() {
+                const test = this.$refs.test;
+                
+                try {
+                    await API.requestLogged("GET", "/users/", undefined, undefined);
+                    console.log("OK");
+                    
+                } catch (error) {
+                    const json = await error.json();
+                    
+                }
+                
+            }
+        },
+        mounted() {
+            this.get_users();
+        }
+    
+    })
 </script>

872f4a27c31fc0f55f4c13e496ee8223d3f358ad
Clement.JL <118436575+aildontknow@users.noreply.github.com>
2023-03-27 22:43:17 +0200

    Update History
    Liaison avec le back. Juste à changer affichage des dates et à ajouter le chemin pour page de notation.

--- crocojourney/src/views/History.vue
+++ crocojourney/src/views/History.vue
@@ -9,48 +9,56 @@
         <div class="row">
             <div class="container-fluid" style="margin-top: 2%;">
                 <figure class="text-center">
-                    <p class="h2">Mes trajets en tant que passager</p>
+                    <p class="h2" style="text-decoration: underline;">Mes trajets en tant que passager</p>
                 </figure>
                 <div class="col-md-8" style="width: auto; margin-left: 1%; margin-right: 1%;">
                     <div class="scrollable">
-                        <div class="card">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <h4 class="card-title" style="margin: auto">Titre:</h4>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="container-fluid">
-                                            <div class="row">
-                                                <div class="col-10">
-                                                    <div class="container-fluid">
-                                                        <div class="row" style="margin-top: 1%;">
-                                                            <div class="col-md-6">
-                                                                <p class="card-text">Ville départ:</p>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <p class="card-text">Ville d'arrivée:</p>
-                                                            </div>
+                        <div v-if="tripsPassenger.length === 0" class="card">
+                            <h5 class="card-title" style="margin: auto;">Vous n'avez pas encore fait de trajets en tant que passager</h5>
+                        </div>
+                        <span v-else>
+                            <div v-for="trip in tripsPassenger" class="card">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="container-fluid">
+                                                <div class="row">
+                                                    <div class="col-10">
+                                                        <div class="col-md-12">
+                                                            <h4 class="card-title" style="margin: auto">Titre: {{trip.title}}</h4>
                                                         </div>
-                                                        <div class="row" style="margin-bottom: 1%;">
-                                                            <div class="col-md-12">
-                                                                <p class="card-text">Date départ:</p>
+                                                        <div class="container-fluid">
+                                                            <div style="display: flex; flex-wrap: wrap;">
+                                                                <div style="width: 70%; margin-top: 1%; margin-bottom: 1%;">
+                                                                    <div>
+                                                                        <p class="card-text" style="font-size: large;"><span class="fw-bold me-2">•</span>Ville départ: {{trip.departure__name}}</p>
+                                                                    </div>
+                                                                    <div>
+                                                                        <p class="card-text" style="font-size: large;"><span class="fw-bold me-2">•</span>Ville d'arrivée: {{trip.arrival__name}}</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div style="width: 30%; margin-top: 1%;">
+                                                                    <p class="card-text" style="font-size: large;"><span class="fw-bold me-2">•</span>Date départ: {{trip.date}}</p>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col">
-                                                    
-                                                </div>
-                                                <div class="col">
-                                                    <a href="#" class="btn btn-success">Noter</a>
+                                                    <div v-if="trip.private" class="col d-flex justify-content-center align-items-center">
+                                                        <i class="bi bi-person-fill-lock" style="font-size: 2.5rem;" title="Ce trajet est privé"></i>
+                                                    </div>
+                                                    <div v-else class="col d-flex justify-content-center align-items-center">
+                                                        <i class="bi bi-person-fill" style="font-size: 2.5rem;" title="Ce trajet est public"></i>
+                                                    </div>
+                                                    <div class="col d-flex justify-content-center align-items-center">
+                                                        <RouterLink to="#" class="btn btn-success align-middle">Noter</RouterLink>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>                        
+                            </div>   
+                        </span>                     
                     </div>
                 </div>
             </div>
@@ -59,48 +67,56 @@
         <div class="row">
             <div class="container-fluid" style="margin-top: 4%;">
                 <figure class="text-center">
-                    <p class="h2">Mes trajets en tant que conducteur</p>
+                    <p class="h2" style="text-decoration: underline;">Mes trajets en tant que conducteur</p>
                 </figure>
                 <div class="col-md-8" style="width: auto; margin-left: 1%; margin-right: 1%;">
                     <div class="scrollable">
-                        <div class="card">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <h4 class="card-title" style="margin: auto">Titre:</h4>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="container-fluid">
-                                            <div class="row">
-                                                <div class="col-10">
-                                                    <div class="container-fluid">
-                                                        <div class="row" style="margin-top: 1%;">
-                                                            <div class="col-md-6">
-                                                                <p class="card-text">Ville départ:</p>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <p class="card-text">Ville d'arrivée:</p>
-                                                            </div>
+                        <div v-if="tripsDriver.length === 0" class="card">
+                            <h5 class="card-title" style="margin: auto;">Vous n'avez pas encore fait de trajets en tant que conducteur</h5>
+                        </div>
+                        <span v-else>
+                            <div v-for="trip in tripsDriver" class="card">
+                                <div class="container-fluid">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="container-fluid">
+                                                <div class="row">
+                                                    <div class="col-10">
+                                                        <div class="col-md-12">
+                                                            <h4 class="card-title" style="margin: auto">Titre: {{trip.title}}</h4>
                                                         </div>
-                                                        <div class="row" style="margin-bottom: 1%;">
-                                                            <div class="col-md-12">
-                                                                <p class="card-text">Date départ:</p>
+                                                        <div class="container-fluid">
+                                                            <div style="display: flex; flex-wrap: wrap;">
+                                                                <div style="width: 70%; margin-top: 1%; margin-bottom: 1%;">
+                                                                    <div>
+                                                                        <p class="card-text" style="font-size: large;"><span class="fw-bold me-2">•</span>Ville départ: {{trip.departure__name}}</p>
+                                                                    </div>
+                                                                    <div>
+                                                                        <p class="card-text" style="font-size: large;"><span class="fw-bold me-2">•</span>Ville d'arrivée: {{trip.arrival__name}}</p>
+                                                                    </div>
+                                                                </div>
+                                                                <div style="width: 30%; margin-top: 1%;">
+                                                                    <p class="card-text" style="font-size: large;"><span class="fw-bold me-2">•</span>Date départ: {{trip.date}}</p>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col">
-                                                    
-                                                </div>
-                                                <div class="col">
-                                                    <a href="#" class="btn btn-success">Noter</a>
+                                                    <div v-if="trip.private" class="col d-flex justify-content-center align-items-center">
+                                                        <i class="bi bi-person-fill-lock" style="font-size: 2.5rem;" title="Ce trajet est privé"></i>
+                                                    </div>
+                                                    <div v-else class="col d-flex justify-content-center align-items-center">
+                                                        <i class="bi bi-person-fill" style="font-size: 2.5rem;" title="Ce trajet est public"></i>
+                                                    </div>
+                                                    <div class="col d-flex justify-content-center align-items-center">
+                                                        <RouterLink to="#" class="btn btn-success align-middle">Noter</RouterLink>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>                        
+                            </div>   
+                        </span>                     
                     </div>
                 </div>
             </div>
@@ -109,9 +125,35 @@
     </div>
 </template>
 
+<script>
+import { defineComponent } from 'vue'
+import API from "../scripts/API.js"
+export default defineComponent({
+  name: 'History',
+  async mounted() {
+    this.getTrips();
+  },
+  data() {
+    // si l'utilisateur n'a pas de photo de profil, on affiche l'image par défaut
+    return {
+        tripsDriver: [],
+        tripsPassenger: [],
+        dateFormat:''
+    }
+  },
+  methods: {
+    async getTrips(){
+        const res = await API.requestLogged(API.METHOD.GET,'/trips/me',undefined,undefined);
+        this.tripsDriver = res.tripsDriver;
+        this.tripsPassenger = res.tripsPassenger;
+    }
+  }
+})
+</script>
+
 <style>
-    .scrollable{
-      height: calc(50vh - 3.5rem);
-      overflow-y: auto;
-    }    
+.scrollable{
+    height: calc(50vh - 3.5rem);
+    overflow-y: auto;
+}
 </style>

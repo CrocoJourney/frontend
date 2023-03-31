4376e13a6b802ef32c8ebfe71700783f956b6a60
Phil1010 <philliptysoun@gmail.com>
2023-03-31 02:13:02 +0200

    Ajout : accepter/refuser passager, participation trajet.

---
+++ crocojourney/src/components/Passagers.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <div class="text-center">
+                <div class="row mb-4 align-items-center">
+                    <h2>{{ this.firstname }} {{ this.lastname }}</h2>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-6 col-md-3">
+                        <label for="formPhoto" class="form-label"></label>
+                        <img :src="photoUrl" class="rounded-circle" width="50" height="50" alt="PHOTO" id="formPhoto" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import API from '../scripts/API';
+
+export default defineComponent({
+    name: 'Passagers',
+    data() {
+        return {
+            photoUrl: '',
+        };
+    },
+    mounted() {
+        this.photoUrl = `${API.API_URL}/static/pictures/` + (this.photoPath ?? 'default.png');
+    },
+    props: {
+        id: {
+            type: Number,
+            required: true,
+        },
+        firstname: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        photoPath: {
+            type: String,
+            required: true,
+        },
+        tripId: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script>
--- crocojourney/src/views/DetailTrip.vue
+++ crocojourney/src/views/DetailTrip.vue
@@ -1,16 +1,11 @@
 <template>
-        <div v-if="showSuccess" class="alert alert-success">Votre demande à été envoyé.</div>
-    <div v-if="showError" class="alert alert-danger">Erreur, le trajet est déjà complet ou vous avez déjà demandé à rejoindre le trajet.</div>
-    <div class="d-flex justify-content-center align-items-center vh-75 p-4">
-      
+  <div class="d-flex justify-content-center align-items-center vh-100 p-3">
   <div class="card border-success">
-    
     <div class="card-header bg-transparent border-success text-center">
-      
-      Details du trajet {{ title }}
+          <h3>Details du trajet {{ title }}</h3>
+
     </div>
     <div class="card-body text">
-      
       <div class="text-center">
         <img :src="photoPath" class="rounded-circle" width="50" height="50" id="formPhoto">
         <br>
@@ -26,7 +21,7 @@
         <br>
         <strong>Départ :</strong> {{ departure.name }}
         <br>
-
+        <strong v-if="this.steps.length>0">Etapes :</strong>
         <ul>
           <li v-for="(step, index) in steps" :key="index">{{ step.name }}</li>
         </ul>
@@ -36,107 +31,169 @@
         <br>
         <strong>Nombre de places :</strong> {{ size }}
         <br>
-        <strong>Contraintes :</strong> {{ constraints }}
+        <strong v-if="this.constraints!==''">Contraintes :</strong> {{ constraints }}
         <br>
-        <strong>Précisions :</strong> {{ precisions }}
+        <strong v-if="this.precisions!==''">Précisions :</strong> {{ precisions }}
+        <br>
+        <strong v-if="this.group!==null">Groupe :</strong> {{ group }}
+
       </p>
     </div>
     <div class="card-footer bg-transparent border-success text-center">
-      <button class="btn btn-success" @click="joinTrip()">Réserver une place</button>
+      <button v-if="driver.id === user.id" type="button" class="btn btn-outline-success me-2" @click="editTrip()">Modifier</button>
+      <button v-if="driver.id === user.id" type="button" class="btn btn-outline-danger me-2" @click="deleteTrip()">Supprimer</button>
+      <button v-if="this.passengers.map(p => p.id).includes(user.id)" class="btn btn-danger" @click="leaveTrip()">Quitter</button>
+      <button v-else-if="this.candidates.map(c => c.id).includes(user.id)" class="btn btn-danger" @click="cancelRequest()">Annuler la demande</button>
+      <button v-else-if="this.user.id !== driver.id" class="btn btn-success" @click="joinTrip()">Demander à rejoindre le trajet</button>
     </div>
-    
   </div>
-  
+</div>
+
+<div class="container my-3 mt-3 mb-3">
+  <div class="row">
+    <div class="col-md-6">
+      <h3>Candidats</h3>
+      <div v-for="candidate in candidates" :key="candidate.id">
+        <Candidats :id="candidate.id" :firstname="candidate.firstname" :lastname="candidate.lastname" :photoPath="candidate.photoPath" :tripId="id" :driver="this.driver.id"/>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <h3>Passagers</h3>
+      <div v-for="passenger in passengers" :key="passenger.id">
+        <Passagers :id="passenger.id" :firstname="passenger.firstname" :lastname="passenger.lastname" :photoPath="passenger.photoPath" :tripId="id" />
+      </div>
+    </div>
+  </div>
 </div>
 
 </template>
 
 <script>
 import API from '../scripts/API';
-export default {
-    name: 'DetailTrip',
-    data() {
-        return {
-          successMessage: '',
-      errorMessage: '',
-      showSuccess: false,
-      showError: false,
-          message: '',
-      error: '',
-            id: '',
-            departure: '',
-            arrival: '',
-            date: '',
-            title: '',
-            rate: '',
-            price: '',
-            size: '',
-            constraints: '',
-            precisions: '',
-            private: '',
-            steps: [
-                {
-                    city_id: '',
-                    order: '',
-                },
-            ],
-            driver: {
-                id: '',
-                mail: '',
-                firstname: '',
-                lastname: '',
-                sex: '',
-                photoPath: '',
-            },
-            group: '',
-            passengers: [],
-            candidates: [],
-            photoPath: '',
-        };
-    },
-    mounted() {
-        this.id = this.$route.params.id;
-        API.requestLogged(API.METHOD.GET, '/trips/' + this.id)
-            .then((data) => {
-                const trip = data;
-                this.departure = trip.departure;
-                this.arrival = trip.arrival;
-                this.date = trip.date;
-                this.title = trip.title;
-                this.rate = trip.rate;
-                this.price = trip.price;
-                this.size = trip.size;
-                this.constraints = trip.constraints;
-                this.precisions = trip.precisions;
-                this.private = trip.private;
-                this.steps = trip.steps;
-                this.driver = trip.driver;
-                this.group = trip.group;
-                this.passengers = trip.passengers;
-                this.candidates = trip.candidates;
-                this.photoPath = `${API.API_URL}/static/pictures/`+ (this.driver.photoPath ?? 'default.png');
-            })
-            .catch((error) => console.log(error));
-    },
-    methods: {
-      joinTrip() {
-      API.requestLogged(API.METHOD.POST, '/trips/' + this.id + '/join')
-        .then((data) => {
-          this.successMessage = data.message;
-          this.showSuccess = true;
-          setTimeout(() => {
-            this.showSuccess = false;
-          }, 2000);
-        })
-        .catch((error) => {
-          this.errorMessage = error.message;
-          this.showError = true;
-          setTimeout(() => {
-            this.showError = false;
-          }, 2000);
-        });
-}
+import User from '../scripts/User';
+import Candidats from '../components/Candidats.vue';
+import Passagers from '../components/Passagers.vue';
 
+export default {
+  name: 'DetailTrip',
+  components: {
+    Candidats,
+    Passagers,
+  },
+  data() {
+    return {
+      user: User.currentUser,
+      id: '',
+      departure: '',
+      arrival: '',
+      date: '',
+      title: '',
+      rate: '',
+      price: '',
+      size: '',
+      constraints: '',
+      precisions: '',
+      private: '',
+      steps: [
+        {
+          city_id: '',
+          order: '',
+        },
+      ],
+      driver: {
+        id: '',
+        mail: '',
+        firstname: '',
+        lastname: '',
+        sex: '',
+        photoPath: '',
+      },
+      group: '',
+      passengers: [],
+      candidates: [],
+      photoPath: '',
+    };
+  },
+  mounted() {
+    this.id = this.$route.params.id;
+    API.requestLogged(API.METHOD.GET, '/trips/' + this.id)
+      .then((data) => {
+        const trip = data;
+        this.departure = trip.departure;
+        this.arrival = trip.arrival;
+        this.date = trip.date;
+        this.title = trip.title;
+        this.rate = trip.rate;
+        this.price = trip.price;
+        this.size = trip.size;
+        this.constraints = trip.constraints;
+        this.precisions = trip.precisions;
+        this.private = trip.private;
+        this.steps = trip.steps;
+        this.driver = trip.driver;
+        this.group = trip.group;
+        this.passengers = trip.passengers;
+        this.candidates = trip.candidates;
+        this.photoPath = `${API.API_URL}/static/pictures/` + (this.driver.photoPath ?? 'default.png');        
+      })
+      .catch((error) => console.log(error));
+
+  },
+  methods: {
+    async joinTrip() {
+      try {
+        await API.requestLogged(API.METHOD.POST, '/trips/' + this.id + '/join');
+        alert('Votre demande à été envoyé !');
+        window.location.reload();
+      } catch (error) {
+        alert('Erreur lors de la demande de trajet.');
+      }
     },
+
+    async leaveTrip() {
+      try {
+        await API.requestLogged(API.METHOD.DELETE, '/trips/' + this.id + '/passengers/' + this.user.id);
+        alert('Vous avez quitté le trajet avec succès !');
+        window.location.reload();
+      } catch (error) {
+        alert('Erreur lors de la tentative de quitter le trajet.');
+      }
+    },
+
+    async deleteTrip() {
+      try {
+        await API.requestLogged(API.METHOD.DELETE, '/trips/' + this.id);
+
+        // réinitialiser les données du composant
+        this.trips = [];
+
+        // rediriger l'utilisateur vers la page précédente
+        window.history.back();
+        alert('Le trajet a été supprimé avec succès !');
+      } catch (error) {
+        alert('Erreur lors de la tentative de supprimer le trajet.');
+      }
+    },
+
+    async cancelRequest() {
+      try {
+        await API.requestLogged(API.METHOD.DELETE, '/trips/' + this.id + '/candidates/' + this.user.id);
+        alert('Votre demande a été annulée avec succès !');
+        window.location.reload();
+      } catch (error) {
+        alert('Erreur lors de la tentative d\'annulation de votre demande.');
+      }
+    },
+
+    async editTrip() {
+      try {
+        await API.requestLogged(API.METHOD.PATCH, '/trips/' + this.id);
+        alert('Le trajet a été modifié avec succès !');
+      } catch (error) {
+        alert('Erreur lors de la tentative de modification du trajet.');
+      }
+    },
+
+  },
 };
 </script>
